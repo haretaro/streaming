c1868c89a72212559dfae28eac86922f8cd15aa6
haretaro <frontend.1000@gmail.com>
2016-05-20 00:43:41 +0900

    コマンドライン引数で設定を変えられるようにした

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
+#coding: utf-8
 import tornado.ioloop
 import tornado.web
 import io
 import cv2
 import picap
+import argparse
 
-cap = picap.get_capturer(False, resolution=(480,320))
+parser = argparse.ArgumentParser(description='webカメラとかからストリーミングするスクリプト')
+parser.add_argument('-c', '--camera_module',
+        help='Raspi のカメラモジュールを使用する',
+        action='store_true')
+parser.add_argument('-r', '--resolution',
+        help='解像度.横,高さ',
+        nargs=2,
+        type=int,
+        metavar='X',
+        default=[320,240])
+args = parser.parse_args()
+
+cap = picap.get_capturer(args.camera_module, resolution=args.resolution)
 
 class rootpage(tornado.web.RequestHandler):
     def get(self):
